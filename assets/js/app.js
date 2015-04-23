2309ecc3871ba1465aa6804efa3a36afa5e07dfc
lukesmmr <lukas@goodthingseverywhere.com>
2015-04-23 17:01:12 -0300

    adjust form validation to get error messages from JSON

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+
+	// get forms json
+	var jsonData = $.parseJSON($.ajax({
+        url:  "content/forms.json",
+        async: false
+    }).responseText);
+
+	var submit_btn = jsonData.fields.submit;
+	var required = jsonData.messages.required;
+    var email_req = jsonData.messages.email_required;
+    var error_msg = jsonData.messages.error_msg;
+    var error_btn = jsonData.messages.error_btn;
+    //console.log(email_req);
+
 	// navi classes
 	$('body.about nav.navbar li.about').addClass('active');
 
@@ -13,7 +27,11 @@ $(document).ready(function () {
 	// submit newsletter
 	$("#newsletter-submit").validate({
 		rules: {email: {required: true, email: true}},
-		messages: {email: "Please enter a valid email address"},
+		messages: {
+			firstname: required,
+			lastname: required,
+			email: email_req
+		},
 		submitHandler: function(form) {
 	        $('.submit-success').remove();
 	        $('.submit-btn').attr('disabled', 'disabled').html('<img style="height: 40px;" src="assets/img/loader-puff.svg">');
@@ -24,12 +42,12 @@ $(document).ready(function () {
 	            data: $(form).serialize(),
 	            success: function (data) {
 	                $(form).append('<span class="submit-success"> ' + data + '</span>');
-	                $('.submit-btn').html('Submit').removeAttr('disabled');
+	                $('.submit-btn').html(submit_btn).removeAttr('disabled');
 	                $('input').val('');
 	            },
 	            error: function (xhr, ajaxOptions, thrownError) {
-	                $('.submit-btn').val('Submit again');
-	                $(form).append('Error, please try again.');
+	                $('.submit-btn').val(error_btn);
+	                $(form).append(error_msg);
 	            	$('input').val('');
 	            }
 	        })
@@ -39,7 +57,13 @@ $(document).ready(function () {
 	// submit contact form
 	$("#contact-form").validate({
 		rules: {email: {required: true, email: true}},
-		messages: {email: "Please enter a valid email address"},
+		messages: {
+			firstname: required,
+			lastname: required,
+			email: email_req,
+			message: required,
+			reason_interested: required
+		},
 		submitHandler: function(form) {
 	        $('.submit-success').remove();
 	        $('.submit-btn').attr('disabled', 'disabled').html('<img style="height: 40px;" src="assets/img/loader-puff.svg">');
@@ -50,12 +74,12 @@ $(document).ready(function () {
 	            data: $(form).serialize(),
 	            success: function (data) {
 	                $('.submit-btn').before('<span class="submit-success"> ' + data + '</span>');
-	                $('.submit-btn').html('Submit').removeAttr('disabled');
+	                $('.submit-btn').html(submit_btn).removeAttr('disabled');
 	                $('input').val('');
 	            },
 	            error: function (xhr, ajaxOptions, thrownError) {
-	                $('.submit-btn').val('Submit again');
-	                $('.submit-btn').before(' Error');
+	                $('.submit-btn').val(error_btn);
+	                $('.submit-btn').before(error_msg);
 	            	$('input').val('');
 	            }
 	        });
