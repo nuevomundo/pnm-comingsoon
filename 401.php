85e4c208e230d0d3eed887067ad607135e925a8d
lukesmmr <lukas@goodthingseverywhere.com>
2015-04-24 17:24:36 -0300

    add 401 error page

---
+++ 401.php
@@ -0,0 +1,12 @@
+<?php $page_url = "/401.php"; ?>
+<?php include('views/head.php'); ?>
+<body class="generic">
+	<?php include('views/header.php'); ?>
+	<div class="container">
+    	<div class="row">
+			<?php include('views/content-401.php') ?>
+		</div>
+	</div>
+	<?php include('views/footer.php'); ?>
+</body>
+</html>
---
+++ views/content-401.php
@@ -0,0 +1,7 @@
+		<section class="row grid error404">
+		    <div class="col-md-12">
+		    	<h1>401</h1>
+				<h2>Unauthorized - Your authorization failed.</h2>
+				<p>Please try refreshing the page and fill in the correct login details.</p>
+			</div>
+		</section>
