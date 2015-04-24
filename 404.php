ef8ee01470a79d959ee5106fa50f94c05d18f6aa
lukesmmr <lukas@goodthingseverywhere.com>
2015-04-23 22:39:18 -0300

    Adding error pages

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php $page_url = "/404.php"; ?>
+<?php include('views/head.php'); ?>
+<body class="generic">
+	<?php include('views/header.php'); ?>
+	<div class="container">
+    	<div class="row">
+			<?php include('views/content-404.php') ?>
+		</div>
+	</div>
+	<?php include('views/footer.php'); ?>
+</body>
+</html>
---
+++ 500.php
@@ -0,0 +1,12 @@
+<?php $page_url = "/500.php"; ?>
+<?php include('views/head.php'); ?>
+<body class="generic">
+	<?php include('views/header.php'); ?>
+	<div class="container">
+    	<div class="row">
+			<?php include('views/content-500.php') ?>
+		</div>
+	</div>
+	<?php include('views/footer.php'); ?>
+</body>
+</html>
