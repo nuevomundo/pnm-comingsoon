da885f940fb174d2bf036538c5e459e115a173e8
lukesmmr <lukas@goodthingseverywhere.com>
2015-04-24 13:56:10 -0300

    get local jquery

--- views/footer.php
+++ views/footer.php
@@ -14,7 +14,7 @@
 </footer>
 <?php //include('modal.php'); ?>
 
-<script src='http://code.jquery.com/jquery-1.10.2.min.js'></script>
+<script src='assets/js/jquery-1.11.2.min.js'></script>
 <script src='assets/js/plugins.min.js'></script>
 <script src='assets/js/app.min.js'></script>
 <?php if ($pnm["config"]["analytics"]): ?>
