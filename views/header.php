8832e9372b96948aa3ae237076cab3ab37576e49
Lukas <lukas@goodthingseverywhere.com>
2015-07-03 13:33:54 +0100

    update blog link

--- views/header.php
+++ views/header.php
@@ -17,7 +17,7 @@
         <ul class="nav navbar-nav">
           <!-- <li class="centers"><a href="#">Centers</a></li> -->
           <li class="about"><a href="about.php<?php if( isset($_GET['lang']) && $lang ) : echo "?lang=". $lang; endif; ?>"><?php echo $navi[$lang]['about']; ?></a></li>
-          <li class="blog"><a href="http://numundo.org/blog" target="_blank"><?php echo $navi[$lang]['blog']; ?></a></li>
+          <li class="blog"><a href="http://blog.numundo.org/" target="_blank"><?php echo $navi[$lang]['blog']; ?></a></li>
           <li class="list-btn">
               <a href="https://pnm.typeform.com/to/UHaXXV" target="_blank"><button class="btn" type="button"><?php echo $navi[$lang]['center']; ?></button></a>
           </li>
